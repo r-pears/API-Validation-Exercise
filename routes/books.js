0c1d7317dc45502d9d206f142a18b3f58124c966
Ryan <ryjen.pears@gmail.com>
2022-04-01 19:11:53 +0200

    book route updated

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,13 @@
+// Book routes
+
 const express = require("express");
 const Book = require("../models/book");
 
 const router = new express.Router();
 
+const { validate } = require("jsonschema");
+const bookSchemaNew = require("../schemas/bookSchemaNew");
+const bookSchemaUpdate = require("../schemas/bookSchemaUpdate");
 
 /** GET / => {books: [book, ...]}  */
 
@@ -17,9 +22,9 @@ router.get("/", async function (req, res, next) {
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
+router.get("/:isbn", async function (req, res, next) {
   try {
-    const book = await Book.findOne(req.params.id);
+    const book = await Book.findOne(req.params.isbn);
     return res.json({ book });
   } catch (err) {
     return next(err);
@@ -30,6 +35,13 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const validation = validate(req.body, bookSchemaNew);
+    if (!validation.valid) {
+      return next({
+        status: 400,
+        error: validation.errors.map(error => error.stack)
+      });
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
@@ -41,6 +53,21 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    if ("isbn" in req.body) {
+      return next({
+        status: 400,
+        message: "Not allowed"
+      });
+    }
+
+    const validation = validate(req.body, bookSchemaUpdate);
+    if (validation.valid) {
+      return next({
+        status: 400,
+        errors: validation.errors.map(error => error.stack)
+      });
+    }
+    
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
